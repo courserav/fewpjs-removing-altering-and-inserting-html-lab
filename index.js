a3fee6bc1467abfbee2f684ac107337b63630b2a
courserav <lvsmlit@gmail.com>
2021-07-31 15:23:38 -0400

    commit tree 7ae1927f78fd82d233cb18af2d4020eedbb28c0b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let mainElement = document.getElementById('main')
+mainElement.remove()
+
+let newHeader = document.getElementById('victory')
+newHeader.innerHTML = 'YOUR-NAME is the champion'
